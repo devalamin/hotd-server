965e43224c3535cabbfa97a930aff45f3a08a212
devalamin <alamin.hasibmd2017@gmail.com>
2022-10-21 00:37:47 +0600

    hotd server

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+
+
+const categories = require('./data/categories.json');
+const news = require('./data/news.json')
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('new server api running');
+});
+
+app.get('/categories', (req, res) => {
+    res.send(categories)
+})
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news)
+    }
+    else {
+        const selectedCategory = news.filter(n => n.category_id === id);
+        res.send(selectedCategory)
+
+    }
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+    console.log(req.params)
+})
+
+app.listen(port, () => {
+    console.log('server running on port', port);
+})
